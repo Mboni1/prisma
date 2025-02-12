ce746793f2899ae2246147aac310a5cf9d9e34c9
Mboni1 <mbonimpaclaude27@gmail.com>
2025-02-12 11:03:31 +0200

    database

--- script.ts
+++ script.ts
@@ -2,23 +2,21 @@ import { PrismaClient } from "@prisma/client"
 const prisma = new PrismaClient()
 
 async function main() {
-   const user = await prisma.user.create({
-      data: {
-        name: "claude",
-        email: "closmbonimpa1@gmail.comoo",
-        age: 32,
-       userPreference: {
-        create: {
-          emailUpdates: true,
+ const user = await prisma.user.update ({
+      where: {
+        email: "closmbonimpa@gmail.com",
         },
-       },
+      data: {
+        userPreference: {
+          connect: {
+            id: '3f1a78a2 - 74d7- 4b34- b91c - 718db77c69ea'
+          }
+        }
       },
-      include: {
-        userPreference: true,
-      },
-    })
-  console.log(user)
-}
+     })
+     console.log(user)
+  }
+
 
 main()
  .catch(e => {
